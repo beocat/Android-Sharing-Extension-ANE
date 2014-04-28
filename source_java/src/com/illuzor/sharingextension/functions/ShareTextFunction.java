e33ef525e51073fe52f70f7c80085cee1b859c5c
Magnus Poppel <magnuspoppel@gmail.com>
2014-04-28 20:17:56 +0200

    Forced twitter in sharetextfunction
    Instead of creating a list of all apps that can share, the
    sharetextfunction selects one of the twitter apps if any.

--- source_java/src/com/illuzor/sharingextension/functions/ShareTextFunction.java
+++ source_java/src/com/illuzor/sharingextension/functions/ShareTextFunction.java
@@ -1,8 +1,18 @@
 package com.illuzor.sharingextension.functions;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
+import java.util.List;
+import android.content.Context;
+import android.content.pm.ResolveInfo;
 
 import com.adobe.fre.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.content.pm.PackageInfo;
 
 public class ShareTextFunction implements FREFunction {
 
@@ -22,5 +32,31 @@ public class ShareTextFunction implements FREFunction {
 
 		return null;
 	}
+
+	public Intent findTwitterClient(FREContext context) {
+	    final String[] twitterApps = {
+	            // package // name - nb installs (thousands)
+	            "com.twitter.android", // official - 10 000
+	            "com.twidroid", // twidroid - 5 000
+	            "com.handmark.tweetcaster", // Tweecaster - 5 000
+	            "com.thedeck.android",
+	            "com.dotsandlines.carbon" }; // TweetDeck - 5 000 };
+	    Intent tweetIntent = new Intent();
+	    tweetIntent.setType("text/plain");
+	    final PackageManager packageManager = context.getActivity().getPackageManager();
+	    
+	    List<ResolveInfo> list = packageManager.queryIntentActivities(
+	            tweetIntent, PackageManager.MATCH_DEFAULT_ONLY);
+	    for (int i = 0; i < twitterApps.length; i++) {
+	        for (ResolveInfo resolveInfo : list) {
+	            String p = resolveInfo.activityInfo.packageName;
+	            if (p != null && p.startsWith(twitterApps[i])) {
+	                tweetIntent.setPackage(p);
+	                return tweetIntent;
+	            }
+	        }
+	    }
+   		return null;
+   	}
 
 }
